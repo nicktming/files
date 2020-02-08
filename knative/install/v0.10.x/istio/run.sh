04ebb39ef8a217ee880eea6358772ca1206b582b
nicktming <2279563278@qq.com>
2020-02-08 14:55:09 +0800

    add v0.10.x

---
+++ knative/install/v0.10.x/istio/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export ISTIO_VERSION=1.1.7
+curl -L https://git.io/getLatestIstio | sh -
+cd istio-${ISTIO_VERSION}
+
+for i in install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
+
